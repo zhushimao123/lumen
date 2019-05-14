1681574fb9d3972eaad8d86eb3c2284613140040
zhushimao123 <152359342622@163.com>
2019-05-14 07:13:30 +0000

    ?

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -55,8 +55,8 @@ class ApiController extends BaseController
     //注册
     public function userinfo(Request $request)
     {
-        $a =$request -> input();
-      echo   json_encode($a,true);die;
+        $data =$request -> all();
+      echo   json_encode($data,JSON_UNESCAPED_UNICODE);die;
 //        header('Access-Control-Allow-Origin:http://client.1809a.com');
 //        $json_str = file_get_contents("php://input");
 //         if(empty($_POST)){
