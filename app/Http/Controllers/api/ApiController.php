24cf9012eda1b8cbafc039cc8ffe4fc01d1135cc
zhushimao123 <15235942622@163.com>
2019-05-18 06:46:17 +0000

    提交顶到

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -382,7 +382,7 @@ class ApiController extends BaseController
         $user_id = $_GET['uid'];
         $order_id = $_GET['order_id'];
         $res = DB::table('shop_order')->where(['order_id'=> $order_id,'user_id'=>$user_id])->first();
-        $order_no = $res-> oreder_no;
+        $order_no = $res-> order_no;
         $order_amount = $res-> order_amount;
         $response = [
             'order_no'=> $order_no,
